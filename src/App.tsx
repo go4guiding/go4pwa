ed5f3afad2f8959ef4574d5e07c4a9041906336a
Mark Bailey <markbailey78@yahoo.co.uk>
2022-03-23 14:04:25 +0000

    Updated PrivateRoute with authorisation checks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { Role } from 'common';
 import { RootState, onAuthChanged } from 'store';
 import { useAppDispatch, useAppSelector } from 'hooks';
 
-import { StartPage } from 'pages/public';
-import { SignInPage, SignUpPage } from 'pages/auth';
-import { PageNotFound } from 'pages/error';
 import PrivateRoute from 'components/private-route';
-// import Routes from 'components/routes';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -18,49 +14,45 @@ function App() {
     (state: RootState) => state.auth
   );
 
+  const PR = useMemo(
+    () => (props: any) =>
+      (
+        <PrivateRoute
+          {...props}
+          user={user}
+          isAuthenticating={isAuthenticating}
+        />
+      ),
+    [user, isAuthenticating]
+  );
+
   useEffect(() => onAuthChanged(dispatch), [dispatch]);
 
   return (
     <Routes>
       {/* Public Routes */}
       <Route path="/">
-        <Route index element={<StartPage />} />
-        <Route path="sign-in" element={<SignInPage />} />
-        <Route path="sign-up" element={<SignUpPage />} />
+        <Route index element={<div>{'Start Page'}</div>} />
+        <Route path="sign-in" element={<div>{'Sign In Page'}</div>} />
+        <Route path="sign-up" element={<div>{'Sign Up Page'}</div>} />
       </Route>
 
       {/* Member Routes */}
       <Route path="/members">
-        <Route
-          element={
-            <PrivateRoute
-              user={user}
-              isAuthenticating={isAuthenticating}
-              authorisedRoles={[Role.Member]}
-            />
-          }
-        >
+        <Route element={<PR authorisedRoles={[Role.Member]} />}>
           <Route index element={<div>{'Members Only'}</div>} />
         </Route>
       </Route>
 
       {/* Admin Routes */}
       <Route path="/admin">
-        <Route
-          element={
-            <PrivateRoute
-              user={user}
-              isAuthenticating={isAuthenticating}
-              authorisedRoles={[Role.Admin]}
-            />
-          }
-        >
+        <Route element={<PR authorisedRoles={[Role.Admin]} />}>
           <Route index element={<div>{'Admins Only'}</div>} />
         </Route>
       </Route>
 
       {/* Catch All Route */}
-      <Route path="*" element={<PageNotFound />} />
+      <Route path="*" element={<div>{'Page Not Found'}</div>} />
     </Routes>
   );
 }
--- src/components/private-route/PrivateRoute.tsx
+++ src/components/private-route/PrivateRoute.tsx
@@ -1,4 +1,5 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { Role, UserState } from 'types/auth';
 
 type PrivateRouteProps = {
@@ -9,8 +10,18 @@ type PrivateRouteProps = {
 
 function PrivateRoute(Props: PrivateRouteProps) {
   const { authorisedRoles, user, isAuthenticating } = Props;
-  console.log('PrivateRoute!', Props);
-  return <Outlet />;
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const isUnauthorised =
+    !isAuthenticating && !(user && authorisedRoles.includes(user.role));
+
+  useEffect(() => {
+    if (!isAuthenticating && !user)
+      navigate(`/sign-in?r=${encodeURI(location.pathname)}`);
+  }, [user, isAuthenticating, location, navigate]);
+
+  return !isUnauthorised ? <Outlet /> : <div>{'Unauthorised Access'}</div>;
 }
 
 export default PrivateRoute;
