6a63280540ff903efa10b84944d77e95d0f53747
Mark Bailey <markbailey78@yahoo.co.uk>
2022-03-23 17:35:52 +0000

    Created Spinner component

--- src/components/ui/spinner/Spinner.tsx
+++ src/components/ui/spinner/Spinner.tsx
@@ -1,7 +1,42 @@
+import { HTMLProps } from 'react';
+import { ThemeColour } from 'types/app';
+
+import buildClassName from 'utilities/buildClassName';
 import styles from './spinner.module.scss';
 
-function Spinner() {
-  return <div className={styles.base} />;
+type SpinnerProps = HTMLProps<HTMLDivElement> & {
+  busy?: boolean;
+  label?: string;
+  color?: ThemeColour;
+  large?: boolean;
+};
+
+function Spinner(props: SpinnerProps) {
+  const {
+    className,
+    color,
+    large = false,
+    busy = true,
+    label = 'The content is loading',
+    ...otherProps
+  } = props;
+
+  const size = (large && styles.large) || null;
+  const newClassName = buildClassName(
+    styles[color || 'default'],
+    size,
+    className
+  );
+
+  return (
+    <div
+      {...otherProps}
+      role="status"
+      aria-busy={busy}
+      aria-label={label}
+      className={newClassName}
+    />
+  );
 }
 
 export default Spinner;
