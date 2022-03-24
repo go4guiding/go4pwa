d30456a7a4c573ec05ed1377571fd1578a44e5d3
Mark Bailey <markbailey78@yahoo.co.uk>
2022-03-24 11:24:45 +0000

    Created app type definitions file

---
+++ src/types/app.d.ts
@@ -0,0 +1,49 @@
+export declare type AppState = {
+  isInitialLoad: boolean;
+};
+
+// Colours
+export declare type ThemeColour =
+  | 'primary'
+  | 'primary-70'
+  | 'primary-20'
+  | 'secondary'
+  | 'secondary-70'
+  | 'secondary-20'
+  | 'light'
+  | 'dark'
+  | 'grey'
+  | 'grey-50'
+  | 'danger'
+  | 'danger-light'
+  | 'danger-dark'
+  | 'warning'
+  | 'warning-light'
+  | 'warning-dark'
+  | 'success'
+  | 'success-light'
+  | 'success-dark'
+  | 'info'
+  | 'info-light'
+  | 'info-dark';
+
+export declare type AwardColour = 'bronze' | 'silver' | 'gold';
+export declare type AwardThemeColour =
+  | 'know-myself'
+  | 'know-myself-70'
+  | 'know-myself-20'
+  | 'express-myself'
+  | 'express-myself-70'
+  | 'express-myself-20'
+  | 'be-well'
+  | 'be-well-70'
+  | 'be-well-20'
+  | 'have-adventures'
+  | 'have-adventures-70'
+  | 'have-adventures-20'
+  | 'take-action'
+  | 'take-action-70'
+  | 'take-action-20'
+  | 'skills-for-my-future'
+  | 'skills-for-my-future-70'
+  | 'skills-for-my-future-20';
