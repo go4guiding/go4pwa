e17c10d0afa6acd27da5aff7dcd30a2d9e9b3075
Mark Bailey <markbailey78@yahoo.co.uk>
2022-03-24 08:15:45 +0000

    Created html type definitions

---
+++ src/types/html.d.ts
@@ -0,0 +1,11 @@
+import { DetailedHTMLProps, HTMLAttributes, ButtonHTMLAttributes } from 'react';
+
+export declare type HTMLButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+
+export declare type HTMLDivProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>;
