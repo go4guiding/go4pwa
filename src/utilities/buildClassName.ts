da25b1d680d1b127f8fec9bafd8c2fad492ac828
Mark Bailey <markbailey78@yahoo.co.uk>
2022-03-26 11:29:00 +0000

    Created string utilities file and moved buildClassName function into it

--- src/utilities/buildClassName.ts
+++
@@ -1,7 +0,0 @@
-type BuildClassNameArgs = (string | undefined | null)[];
-
-function buildClassName(...args: BuildClassNameArgs) {
-  return args.filter((arg) => !!arg).join(' ');
-}
-
-export default buildClassName;
