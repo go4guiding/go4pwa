b2c986b107637b960448df4b3bd218a6aeade7a2
Mark Bailey <markbailey78@yahoo.co.uk>
2022-03-28 21:57:15 +0100

    Updated index with auth functions and reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,2 +1,12 @@
 export * from './store';
-export * from './auth';
+export { reducer as appReducer } from './app';
+export {
+  reducer as authReducer,
+  onAuthChanged,
+  onTokenChanged,
+  getSignUpTokenResult,
+  signUp,
+  signIn,
+  signOut,
+  verifyAuth
+} from './auth';
