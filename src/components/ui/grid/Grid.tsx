5eaab3d9d1a4b6422bca7799525208e176c2185c
Mark Bailey <markbailey78@yahoo.co.uk>
2022-04-19 08:29:03 +0100

    Updated Grid columns property to handle object of breakpoints

--- src/components/ui/grid/Grid.tsx
+++ src/components/ui/grid/Grid.tsx
@@ -4,9 +4,19 @@ import { HTMLDivProps } from 'types/html';
 import { buildClassName } from 'utilities/string';
 import styles from './grid.module.scss';
 
+export const gridStyles = styles;
+
+export type GridColumnOptions = {
+  xs?: number;
+  sm?: number;
+  md?: number;
+  lg?: number;
+  xl?: number;
+};
+
 type GridProps = HTMLDivProps &
   PropsWithChildren<{
-    columns: number;
+    columns: number | GridColumnOptions;
     noGutters?: boolean;
     collapse?: boolean;
   }>;
@@ -15,12 +25,26 @@ function Grid(props: GridProps) {
   const { children, className, columns, noGutters, collapse, ...otherProps } =
     props;
 
-  const columnClassName = styles[`columns-${columns}`];
-  const gutterClassName = (noGutters && styles[`no-gutters`]) || null;
+  const columnClassName =
+    typeof columns === 'number'
+      ? styles[`columns_${columns}`]
+      : Object.keys(columns).reduce(
+          (acc, key) =>
+            acc.concat([
+              styles[
+                `columns_${key}_${(columns as Record<string, number>)[key]}`
+              ]
+            ]),
+          [] as string[]
+        );
+
+  const gutterClassName = (noGutters && styles.noGutters) || null;
   const collapseClassName = (collapse && styles.collapse) || null;
   const newClassName = buildClassName(
     styles.container,
-    columnClassName,
+    ...(typeof columnClassName === 'string'
+      ? [columnClassName]
+      : columnClassName),
     gutterClassName,
     collapseClassName,
     className
--- src/components/ui/grid/index.ts
+++ src/components/ui/grid/index.ts
@@ -1,2 +1,3 @@
 import Grid from './Grid';
+export { gridStyles } from './Grid';
 export default Grid;
