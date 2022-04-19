89871e50667c8cb082ad05d4837d09661e8daa3d
Mark Bailey <markbailey78@yahoo.co.uk>
2022-04-19 14:21:19 +0100

    Updated common with breakpoints

--- src/common.ts
+++ src/common.ts
@@ -10,3 +10,20 @@ export enum Transition {
   SlideUp = 'slide-up',
   SlideDown = 'slide-down'
 }
+
+export enum BreakpointMin {
+  // XS = '0rem',
+  SM = '36rem',
+  MD = '48rem',
+  LG = '62rem',
+  XL = '75rem',
+  XXL = '100rem'
+}
+
+export enum BreakpointMax {
+  XS = '35.9375rem',
+  SM = '47.9375rem',
+  MD = '61.9375rem',
+  LG = '74.9375rem',
+  XL = '99.9375rem'
+}
